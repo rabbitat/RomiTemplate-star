88a6bac4f0a6e18f43e9cbb8e86ba3382af0677e
christimyao <yonderthunder@gmail.com>
2025-12-17 20:14:46 -0500

    christian yao 12/17/25 added auton template for top left corner of board, will edit after test.

--- src/main/java/frc/robot/autons/YourAutonomous.java
+++ src/main/java/frc/robot/autons/YourAutonomous.java
@@ -1,14 +1,25 @@
 package frc.robot.autons;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.DriveDistance;
+import frc.robot.commands.DriveTurn;
+import frc.robot.commands.DriveArc;
 import frc.robot.subsystems.Drivetrain;
 
 public class YourAutonomous extends SequentialCommandGroup {
     public YourAutonomous(Drivetrain drivetrain) {
         addCommands(
-            /* ROOKIES, INSERT DRIVE COMMANDS HERE TO CREATE YOUR AUTON! 
-             * EX: new DriveTime(parameter, parameter, parameter)
-            */
+            /*assuming you start at top left corner
+             * edit parameters after testing
+             * write auton for other scenario later
+             * -christian yao 12/17/25
+             */
+            new DriveDistance(1, 10, drivetrain),
+            new DriveTurn(1, 90, 10, drivetrain),
+            new DriveTurn(1, 270, 10, drivetrain),
+            new DriveTurn(1, 270, 10, drivetrain),
+            new DriveTurn(1, 90, 0, drivetrain),
+            new DriveArc(50, 35, drivetrain, 5)
         );
     }
 }
